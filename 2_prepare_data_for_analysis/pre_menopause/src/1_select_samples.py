35f15e453db515a78b456e8bbb6fc4a62e5e703a
Deepro Banerjee <dzb5732@sarah.bx.psu.edu>
2023-02-17 17:04:33 -0500

    all-groups-combo-code-added

---
+++ 2_prepare_data_for_analysis/pre_menopause/src/1_select_samples.py
@@ -0,0 +1,15 @@
+# for this analysis select white british samples with whole exome sequencing and bmi data
+
+
+
+import pandas as pd
+
+phenotypes_data = "/data5/deepro/ukbiobank/papers/bmi_project/1_parse_data/prepare_general_pheno/data/ukb_phenotypes_general.csv"
+final_samples_file = "/data5/deepro/ukbiobank/papers/bmi_project/2_prepare_data_for_analysis/pre_menopause/data/samples.csv"
+
+phenotype_df = pd.read_csv(phenotypes_data)
+# filter for white british female samples without menopause
+phenotype_df = phenotype_df.loc[(phenotype_df.genetic_ethnic_grouping == 1)&(phenotype_df.genetic_sex == 0)&(phenotype_df.had_menopause == 'No')]
+# filter for samples with BMI data
+phenotype_df = phenotype_df[~phenotype_df.bmi.isna()]
+phenotype_df.to_csv(final_samples_file, index=False)
---
+++ 2_prepare_data_for_analysis/white_british_female/src/3_linear_correction.R
@@ -0,0 +1,29 @@
+#!/bin/R
+library(glue)
+
+final_samples_file = "/data5/deepro/ukbiobank/papers/bmi_project/2_prepare_data_for_analysis/white_british_female/data/samples.csv"
+corrected_file_samples_file = "/data5/deepro/ukbiobank/papers/bmi_project/2_prepare_data_for_analysis/white_british_female/data/samples_with_residuals.csv"
+
+
+df = read.csv(final_samples_file)
+binary_input_columns = c('genetic_sex')
+continuous_discrete_input_columns = c('age_when_attended_assessment_centre')
+pcs = as.character(1:40)
+pcs = glue('PC{pcs}')
+continuous_discrete_input_columns = c(continuous_discrete_input_columns, pcs)
+
+
+# scale the continuous and discrete inputs
+for (col in continuous_discrete_input_columns){
+	df[, glue('{col}_scaled')] = scale(df[, col])
+}
+
+# scale bmi
+df[, 'bmi_scaled'] = scale(df[, 'bmi'])
+formula = glue('bmi_scaled ~ {glue_collapse(binary_input_columns, sep = " + ")} + {glue_collapse(continuous_discrete_input_columns, sep = "_scaled + ")}_scaled')
+mod = lm(formula, data=df, x=T)
+residuals = resid(mod)
+residuals = as.numeric(residuals)
+df$bmi_residuals = residuals
+
+write.csv(df, corrected_file_samples_file, row.names=F)
