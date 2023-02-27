0c89a82c0c43c4e91346bcedb8a6ceb02b22fd10
Deepro Banerjee <dzb5732@sarah.bx.psu.edu>
2023-02-27 10:52:29 -0500

    protective-rarecomb-run-all-data-scripts-in-src-figures-notebooks

---
+++ 4_characterization/white_british/src/0_create_prev_assoc_table.py
@@ -0,0 +1,99 @@
+import pandas as pd
+
+## Turcot et al
+giant_table1 = "/data5/deepro/ukbiobank/papers/bmi_project/0_data_download/published_studies/GIANT/data/Giant_Turcot_Table1.csv"
+giant_table2 = "/data5/deepro/ukbiobank/papers/bmi_project/0_data_download/published_studies/GIANT/data/Giant_Turcot_Table2.csv"
+giant1_df = pd.read_csv(giant_table1)
+giant2_df = pd.read_csv(giant_table2)
+giant_genes = giant1_df['Gene'].to_list() + giant2_df['Gene'].to_list()
+giant_genes = list(set(giant_genes))
+
+## MGI
+mgi_obesity_table = "/data5/deepro/ukbiobank/papers/bmi_project/0_data_download/published_studies/MGI/data/mgi_bmi_obesity_genes.txt"
+mgi_df = pd.read_csv(mgi_obesity_table, header=None)
+mgi_genes = mgi_df[0].to_list()
+
+## GWAS 
+gwas_table = "/data5/deepro/ukbiobank/papers/bmi_project/0_data_download/published_studies/GWAS_Catalog/data/gwas_genes_to_traits.csv"
+gwas_df = pd.read_csv(gwas_table)
+gwas_bmi_obesity_genes = gwas_df.loc[gwas_df.Mapped_Trait.str.contains("body mass index|obesity", regex=True)].Gene.to_list()
+
+## Marenne et al
+marenne_table = "/data5/deepro/ukbiobank/papers/bmi_project/0_data_download/published_studies/Marenne_2020/data/marenne_genes.csv"
+mar_df = pd.read_csv(marenne_table)
+mar_genes = mar_df['Gene'].to_list()
+
+## Akbari et al
+akbari_table = "/data5/deepro/ukbiobank/papers/bmi_project/0_data_download/published_studies/Akbari_2021/data/akbari_genes.csv"
+akb_df = pd.read_csv(akbari_table)
+akb_genes = akb_df.Gene.to_list()
+
+## Locke et al
+locke_table = "/data5/deepro/ukbiobank/papers/bmi_project/0_data_download/published_studies/Locke_2015/data/Locke_genes_parsed.csv"
+locke_df = pd.read_csv(locke_table)
+locke_genes = locke_df.Gene.to_list()
+
+combo_nums = [2, 3]
+
+gencode_file = "/data5/deepro/ukbiobank/papers/bmi_project/1_parse_data/prepare_gencode_genes/data/gencode.v39.parsed.genes.csv"
+gencode = pd.read_csv(gencode_file)
+gencode = gencode[gencode.gene_type == 'protein_coding']
+gencode = gencode.drop_duplicates('gene_id_stripped')
+gencode = gencode.set_index('gene_id_stripped', drop=False)
+
+df = pd.DataFrame()
+for combo_num in combo_nums:
+						
+    statistics_file = f"/data5/deepro/ukbiobank/papers/bmi_project/3_run_rarecomb/white_british/data/parsed_tables/combo_{combo_num}.csv"
+    app = pd.read_csv(statistics_file)
+
+    # app['Comparison'] = app['Phenotype'].apply(lambda s: s.split('_')[0])
+    app['Combo_num'] = combo_num
+
+    app['Group'] = "white british"
+
+    if combo_num == 2:
+        app = app.melt(id_vars=['Phenotype', 'Group', 'Combo_num'], value_vars=['Item_1', 'Item_2'])
+    if combo_num == 3:
+        app = app.melt(id_vars=['Phenotype', 'Group', 'Combo_num'], value_vars=['Item_1', 'Item_2', 'Item_3'])
+
+    app = app.drop('variable', axis=1)
+    app.columns = ['Phenotype', 'Group', 'Combo_num', 'Gene']	
+    app['Group'] = app['Group'] + '_' + app['Phenotype'] + '_' + app['Combo_num'].astype(str)
+    app['Gene_symbol'] = app.Gene.apply(lambda s: gencode.loc[s, 'gene_name'])
+    app = app[['Gene', 'Gene_symbol', 'Group']]
+
+    df = df.append(app)
+
+
+
+
+
+df = df.drop_duplicates()
+
+
+df = df.groupby(['Gene', 'Gene_symbol']).agg(' & '.join)
+df = df.reset_index(drop=False)
+
+
+def compare_genes(gene, gene_trait_dict):
+	if gene in gene_trait_dict.keys():
+		return gene_trait_dict[gene]
+	return ''
+
+df['Giant'] = df['Gene_symbol'].isin(giant_genes)
+df['gwas'] = df['Gene_symbol'].isin(gwas_bmi_obesity_genes)
+df['MGI_obesity'] = df['Gene_symbol'].isin(mgi_genes)
+df['Marenne'] = df['Gene_symbol'].isin(mar_genes)
+df['Akbari'] = df['Gene_symbol'].isin(akb_genes)
+df['Locke'] = df['Gene_symbol'].isin(locke_genes)
+
+
+for col in df.columns[3:]:
+	if col == 'mgi':
+		continue
+	print(col, df[col].sum())
+
+df["any_previous_association"] = (df.iloc[:, 3:]==True).any(axis=1)
+
+df.to_csv("/data5/deepro/ukbiobank/papers/bmi_project/4_characterization/white_british/data/previous_associations/associations.csv", index=False)
---
+++ 4_characterization/white_british_female/src/4a_validation_mean.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+
+
+variants_file = "/data5/deepro/ukbiobank/papers/bmi_project/1_parse_data/annotate_vcf/data/variants_by_gene/lof_missense_pred_freq_0.01.tsv"
+combinations2_file = "/data5/deepro/ukbiobank/papers/bmi_project/3_run_rarecomb/white_british_female/data/parsed_tables/combo_2.csv"
+combinations3_file = "/data5/deepro/ukbiobank/papers/bmi_project/3_run_rarecomb/white_british_female/data/parsed_tables/combo_3.csv"
+phenotypes_file = "/data5/deepro/ukbiobank/papers/bmi_project/2_prepare_data_for_analysis/non_white_british_female/data/samples.csv"
+
+variants_df = pd.read_csv(variants_file, sep="\t", low_memory=False, usecols=["Sample", "variant_id", "Gene", "SYMBOL", "Mut_type"], dtype=str)
+combinations2_df = pd.read_csv(combinations2_file, low_memory=False)
+combinations3_df = pd.read_csv(combinations3_file, low_memory=False)
+phenotypes_df = pd.read_csv(phenotypes_file, low_memory=False, usecols=["eid", "bmi"], dtype={"eid": str, "bmi": np.float64})
+
+# get all single genes
+def get_unique_genes_from_combo_df(combo_df, ncombos):
+    gene_set = set()
+    for i in range(1, ncombos+1):
+        gene_set.update(list(combo_df[f"Item_{i}_symbol"]))
+    return gene_set
+
+
+def get_samples_with_hits(variants_df, gene_set):
+    return set(variants_df.loc[variants_df.SYMBOL.isin(gene_set)].Sample)
+
+def check_combo_func(gene_list, combo_list):
+    for combo in combo_list:
+        if all(gene in gene_list for gene in combo):
+            return True
+    return False
+
+def get_single_combo_phenos(variants_df, phenotypes_df, combinations_dfs, ncombos, ):
+    # get unique genes in combos
+    gene_set = set()
+    all_combos_sorted = []
+    for cdf,nc in zip(combinations_dfs, ncombos):
+        gs = get_unique_genes_from_combo_df(cdf, nc)
+        gene_set.update(gs)
+        # sort combos by their gene names for consistency of pipeline
+        acs = sorted(map(lambda x: tuple(sorted(x)), zip(*[cdf[f"Item_{c}_symbol"].to_list() for c in range(1, nc+1)])))
+        all_combos_sorted.append(acs)
+    all_combos_sorted = sum(all_combos_sorted, [])
+    # store gene to their combo mappings
+    gene_to_combo_map ={g:[] for g in gene_set}
+    for g in gene_set:
+        for c in all_combos_sorted:
+            if g in c:
+                gene_to_combo_map[g].append(c)
+    # get samples with a single hit of the gene and those with combinations
+    all_accepted_samples = set()
+    all_rejected_samples = set()
+    for g in gene_set:
+        # select samples with variants in those genes; these samples may have single or multiple hits
+        samples_list = variants_df.loc[variants_df.SYMBOL.isin([g])].Sample
+        samples_df = variants_df.loc[variants_df.Sample.isin(samples_list)].groupby("Sample").aggregate(lambda x: list(x))
+        # exclude samples which have the combinations for these genes
+        rejected_series = samples_df.SYMBOL.apply(check_combo_func, args=(gene_to_combo_map[g], )) 
+        rejected_samples = samples_df.loc[rejected_series].index.to_list()
+        accepted_samples = samples_df.loc[~rejected_series].index.to_list()
+        all_accepted_samples.update(accepted_samples)
+        all_rejected_samples.update(rejected_samples)
+    # get rid of samples with other combo hits from single gene hits
+    all_accepted_samples =  all_accepted_samples - all_rejected_samples
+    single_hit_pheno = phenotypes_df.loc[phenotypes_df.eid.isin(all_accepted_samples)]
+    combo_hit_pheno = phenotypes_df.loc[phenotypes_df.eid.isin(all_rejected_samples)]
+    non_combo_hit_pheno = phenotypes_df.loc[~phenotypes_df.eid.isin(all_rejected_samples)]
+    return single_hit_pheno, combo_hit_pheno, non_combo_hit_pheno
+
+single_hit_pheno, combo_hit_pheno, non_combo_hit_pheno = get_single_combo_phenos(variants_df, phenotypes_df, (combinations2_df, combinations3_df), (2,3))
+
+combo_hit_pheno["vtype"] = "Combinations"
+non_combo_hit_pheno["vtype"] = "No combinations"
+
+valid_df = pd.concat((non_combo_hit_pheno, combo_hit_pheno), axis=0)
+
+save_table = "/data5/deepro/ukbiobank/papers/bmi_project/4_characterization/white_british_female/data/validation/valid_mean.csv"
+valid_df.to_csv(save_table, index=False)
